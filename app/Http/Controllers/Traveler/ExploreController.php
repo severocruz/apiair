e3d5862c9f797eac35bff15c1a1baff0f49f50a0
fauni <amrfranz@gmail.com>
2025-03-20 12:11:37 -0400

    Detail Accommodation Explore Mapa

--- app/Http/Controllers/Traveler/ExploreController.php
+++ app/Http/Controllers/Traveler/ExploreController.php
@@ -10,6 +10,33 @@ use Exception;
 
 class ExploreController extends Controller{
 
+    public function HandleGetAccommodationById($id){
+        Log::info('Getting accomodation by id');
+        try {
+            $accomodation = Accommodation::with(relations: ['type', 'describe', 'aspects', 'services', 'prices', 'photos'])->where('id', $id)->first();
+            if($accomodation == null){
+                return response()->json(
+                    [
+                           'data'=>[],
+                           'message' => 'No existen anuncios registradas',
+                           'status'    => false], 
+                    200);
+    
+            }
+            return response()->json([
+                'data'=>$accomodation,
+                'status'    => true,
+                'message' => 'Anuncio encontrado'
+            ],200);
+        } catch (Exception $e) {
+            Log::error('Error al obtener el anuncio: '.$e->getMessage());
+            return response()->json([ 
+                'data'=>[],
+                'message' => 'Error al obtener el anuncio',
+                'status'   => false
+            ], 500);
+        }
+    }
     // Buscar anuncios cercanos falta completar el filtro de ubicaciones cercanas
     public function HandleGetNearbyAccomodation(Request $request){
         Log::info('Getting nearby accomodation');
